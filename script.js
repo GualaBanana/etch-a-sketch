936277e6e148c736d17568e53281f36cb28bbba4
GualaBanana <tem4rik@gmail.com>
2021-11-22 00:14:22 +0300

    Add simple color picker.

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ class Canvas {
     this.backgroundColor = "gainsboro";
     this.fill();
   }
-
+  // Figure out how to create one object and append it iteratively or
+  // separately create cells and then iterate through them and modify them as needed in another step. (probably will change performance hit)
   fill() {
     for (let i = 0; i < 100 ** 2; i++) {
       const cell = new Cell(this.backgroundColor);
       this.canvas.appendChild(cell);
     }
   }
+
   clear() {
     // Instead of reseting cells from here consider defining reset() in Cell class.
     const cells = this.canvas.children;
@@ -36,10 +38,6 @@ class Canvas {
   toggleGrid() {
     
   }
-
-  changePaintColor(color) {
-    Canvas.paintColor = color;
-  }
 }
 
 class Cell {
@@ -52,7 +50,10 @@ class Cell {
   }
 }
 
+// Resolve the issue with unreseting input forms after page refresh.
 // Define GUI class that is composed with the canvas on which this GUI operates (likely in another file).
-const canvas = new Canvas(document.querySelector(".canvas"));
+const canvas = new Canvas(document.querySelector("#canvas"));
 const canvasResSlider = document.querySelector("input[name='canvas-res']");
 canvasResSlider.addEventListener("change", (e) => canvas.changeSize(e.target.value));
+const colorPicker = document.querySelector(".color-picker__main-color");
+colorPicker.addEventListener("change", (e) => Canvas.paintColor = e.target.value);
